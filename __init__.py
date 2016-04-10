481b0745329004f1ed6b388ee314e0db6035255c
brando12 <brando12@umbc.edu>
2016-04-10 14:50:45 -0400

    test for OS server error

--- __init__.py
+++ __init__.py
@@ -54,16 +54,17 @@ def store(template,starID):
         #generate a url for each variation
         rootURL = 'static/img/product_images/' + p['sku'] + '/'
 
-        #try:
+        try:
 
-            #for f in os.listdir(rootURL):
-                #if os.path.splitext(f)[1].lower() in ('.jpg', '.jpeg','.png'):
-                    #p['img'] = f
-                    #break
+            for f in os.listdir(rootURL):
+                if os.path.splitext(f)[1].lower() in ('.jpg', '.jpeg','.png'):
+                    p['img'] = f
+                    break
 
-        #except IndexError:
-        p['img'] = ""
-        print 'no image found'
+        except IndexError:
+            print 'no image found'
+        except (err):
+            p['img'] = err
 
         productsFiltered.append(p)
 
