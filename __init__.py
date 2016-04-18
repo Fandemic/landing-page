056b1db1a2844bf05188b552112f712e591771be
brando12 <brando12@umbc.edu>
2016-04-18 11:40:28 -0400

    Trying to get sorting to work

--- __init__.py
+++ __init__.py
@@ -35,7 +35,7 @@ def store(template,starID):
     if star['active']:
         products = star['products']
     else:
-        products = db.sample_products.find({'category': star['category']}).sort("sort",-1)
+        products = db.sample_products.find({'category': star['category']}).sort("sort",-1).limit(30)
 
     #loop products and index the categories on the fly
     catIndex = {}
