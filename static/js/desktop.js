44f2da03ba7c6fc08f1a491e42f25b2fd5ce1138
brando12 <brando12@umbc.edu>
2016-06-08 21:51:35 -0400

    Complete remodel and implementing the stocking wizard

--- static/js/desktop.js
+++ static/js/desktop.js
@@ -109,36 +109,11 @@ $(document).ready(function () {
     $(this).parent().parent().find('.product-picture').attr("src", src);
   });
 
-  //Filter
-  $(function() {
-      //Initialize filterizr with default options
-      $('.filtr-container').filterizr();
-  });
+
 
 });
 
 
-/*DATA FILTERS*/
-$(function() {
-    //Simple filter controls
-    $('.btn-filter').click(function() {
-        $('.btn-filter').removeClass('active');
-        $(this).addClass('active');
-    });
-    //Multifilter controls
-    $('.multifilter li').click(function() {
-        $(this).toggleClass('active');
-    });
-    //Shuffle control
-    $('.shuffle-btn').click(function() {
-        $('.sort-btn').removeClass('active');
-    });
-    //Sort controls
-    $('.sort-btn').click(function() {
-        $('.sort-btn').removeClass('active');
-        $(this).addClass('active');
-    });
-});
 
 /* customer service accordion */
 $('.collapse').on('show.bs.collapse', function() {
