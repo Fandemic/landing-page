02f669828c006462460c73226bc7ab99de2c0737
brando12 <brando12@umbc.edu>
2016-09-17 17:28:57 -0400

    desig changes

--- static/js/builder.js
+++ static/js/builder.js
@@ -103,3 +103,16 @@ $(".thumbnail .content").click(function(){
 
   $.notify("Product Added To Box", { position:"right bottom",className:"success" });
 });
+
+//story wsywig editor
+tinymce.init({
+  selector: 'textarea',
+  height: 500,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste code'
+  ],
+  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  content_css: '//www.tinymce.com/css/codepen.min.css'
+});
