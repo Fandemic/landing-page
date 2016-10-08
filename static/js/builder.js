a4671262b71fa66fcc28a1a182fcf2468a635e26
Estein21 <esteininger21@gmail.com>
2016-10-08 13:09:45 -0400

    random # generator

--- static/js/builder.js
+++ static/js/builder.js
@@ -426,3 +426,16 @@ $(document).ready(function() {
   // For Safari
   return message;
 };
+
+//Random # generator for success modal
+var randomString = function (len, bits)
+{
+    bits = bits || 36;
+    var outStr = "", newStr;
+    while (outStr.length < len)
+    {
+        newStr = Math.random().toString(bits).slice(2);
+        outStr += newStr.slice(0, Math.min(newStr.length, (len - outStr.length)));
+    }
+    return outStr.toUpperCase();
+};
